23233fe1dc84c3b5f9dd20592689395314d0ad05
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-06-14 10:56:35 -0300

    bug: solved dpulication of the search

--- src/view/components/favorite/index.tsx
+++ src/view/components/favorite/index.tsx
@@ -16,6 +16,9 @@ export default function Favorites() {
 
   return (
     <div className="mt-10 flex flex-col gap-5 mb-10">
+      {filteredRepositories.length > 0 && (
+        <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      )}
       {filteredRepositories.length === 0 ? (
         <div className="flex items-center justify-center mt-36">
           <h1 className="text-white font-semibold flex gap-2">
@@ -25,29 +28,23 @@ export default function Favorites() {
         </div>
       ) : (
         filteredRepositories.map((repository) => (
-          <div>
-            <SearchInput
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-            />
-            <div
-              key={repository.id}
-              className="text-white border border-slate-500 p-5 mt-5"
-            >
-              <div className="flex justify-between items-center">
-                <div className="flex gap-2 text-blue-500 font-bold">
-                  {repository.name}
-                  <span className="border rounded-xl p-1 text-[10px] text-white">
-                    {repository.private ? "Private" : "Public"}
-                  </span>
-                </div>
+          <div
+            key={repository.id}
+            className="text-white border border-slate-500 p-5 "
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex gap-2 text-blue-500 font-bold">
+                {repository.name}
+                <span className="border rounded-xl p-1 text-[10px] text-white">
+                  {repository.private ? "Private" : "Public"}
+                </span>
               </div>
-              <span className="text-slate-300 flex mt-2">
-                {repository.description
-                  ? repository.description
-                  : "No description"}
-              </span>
             </div>
+            <span className="text-slate-300 flex mt-2">
+              {repository.description
+                ? repository.description
+                : "No description"}
+            </span>
           </div>
         ))
       )}
